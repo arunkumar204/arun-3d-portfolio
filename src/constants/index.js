fb5a95839cbfceac7092c47d4728c13f47a6c678
Arunkumar <118586803+arunkumar204@users.noreply.github.com>
2024-05-22 10:40:23 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -68,11 +68,6 @@ export const skills = [
         type: "Animation",
     },
     {
-        imageUrl: mui,
-        name: "Material-UI",
-        type: "Frontend",
-    },
-    {
         imageUrl: nextjs,
         name: "Next.js",
         type: "Frontend",
@@ -111,54 +106,41 @@ export const skills = [
 
 export const experiences = [
     {
-        title: "React.js Developer",
-        company_name: "Starbucks",
-        icon: starbucks,
-        iconBg: "#accbe1",
-        date: "March 2022 - April 2023",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "React Native Developer",
-        company_name: "Tesla",
-        icon: tesla,
-        iconBg: "#fbc3bc",
-        date: "Jan 2022 - Dec 2022",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
         title: "Web Developer",
-        company_name: "Shopify",
+        company_name: "Oasis Infobyte",
         icon: shopify,
         iconBg: "#b7e4c7",
-        date: "Jan 2022 - Jan 2024",
+        date: "August 2022 - November 2022",
         points: [
             "Developing and maintaining web applications using React.js and other related technologies.",
             "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
             "Implementing responsive design and ensuring cross-browser compatibility.",
             "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Frontend Developer",
+        company_name: "Edunet",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "June 2023 - August 2023",
+        points: [
+            "(Hyper Text Markup Language) is the most basic building block of the web",
+            "CSS is the coding language used to turn that boring HTML into the beautiful websites that you know and love, but it does so in a very important way without cluttering up your HTML!",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "JavaScript is the only programming language that is used on the front end of websites",
         ],
     },
     {
         title: "Full stack Developer",
-        company_name: "Meta",
+        company_name: "Byte Royalty",
         icon: meta,
         iconBg: "#a2d2ff",
-        date: "Jan 2023 - Present",
+        date: "February 2024 - Present",
         points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Combining frontend and backend skills to create end-to-end web solutions.",
+            "Managing project architecture and database design for seamless integration.",
+            "Optimizing performance and scalability of web applications.",
             "Participating in code reviews and providing constructive feedback to other developers.",
         ],
     },
@@ -173,56 +155,43 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/arunkumar204',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/arunkumar-s-25b335235/',
     }
 ];
 
 export const projects = [
     {
-        iconUrl: pricewise,
-        theme: 'btn-back-red',
-        name: 'Amazon Price Tracker',
-        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-        link: 'https://github.com/adrianhajdin/pricewise',
-    },
-    {
-        iconUrl: threads,
-        theme: 'btn-back-green',
-        name: 'Full Stack Threads Clone',
-        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-        link: 'https://github.com/adrianhajdin/threads',
+        iconUrl: linkedin,
+        theme: 'btn-back-blue',
+        name: 'Linkedin Clone',
+        description: 'a web application built using Tailwind CSS, a utility-first CSS framework. Tailwind CSS simplifies the styling process by providing pre-designed classes that can be easily applied to elements.',
+        link: 'https://arunkumarlinkedin2-o-l8l4.vercel.app/',
     },
     {
         iconUrl: car,
         theme: 'btn-back-blue',
-        name: 'Car Finding App',
-        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+        name: '3D Portfolio',
+        description: 'They create still and moving images and visual effects using computers. These images could be anything from characters to environments to special effects. 3D animations can be created for things like film, TV, or video games.',
+        link: 'https://3d-arunportfolio.vercel.app/',
     },
     {
-        iconUrl: snapgram,
+        iconUrl: game,
         theme: 'btn-back-pink',
-        name: 'Full Stack Instagram Clone',
-        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
-        link: 'https://github.com/adrianhajdin/social_media_app',
+        name: '3D Game',
+        description: '3D gaming refers to interactive computer entertainment games with characteristics of three-dimensional graphics: height, width, and depth. 3D gaming is believed to create immersive experience in a virtual worlds with realistic representations.',
+        link: 'https://master--lighthearted-beijinho-6ee493.netlify.app/',
     },
     {
         iconUrl: estate,
         theme: 'btn-back-black',
-        name: 'Real-Estate Application',
-        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-        link: 'https://github.com/adrianhajdin/projects_realestate',
+        name: 'Chat',
+        description: 'I developed a Gemini clone using React.js that interacts with the Gemini cryptocurrency exchange API. This project showcases my ability to integrate third-party APIs into a modern frontend framework to build a dynamic, data-driven application.',
+        link: 'https://arun-chat.vercel.app/',
     },
-    {
-        iconUrl: summiz,
-        theme: 'btn-back-yellow',
-        name: 'AI Summarizer Application',
-        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
-    }
+    
 ];
